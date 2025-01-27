5d133e72fb4d17e455e1a22a84557028ddf78f91
silv4b <brunno.linkin@gmail.com>
2025-01-27 11:50:28 -0300

    questões da lista de 27-01-2025

--- 2 - Programação Orientada a Objetos/6 - Aula - 2025-24-01/respostas/q8_prod_class.py
+++ 2 - Programação Orientada a Objetos/6 - Aula - 2025-24-01/respostas/q8_prod_class.py
@@ -1,16 +1,28 @@
 # Questão 8: Estoque com manipulação de produtos]
 class Produto:
-    ...
-    # nome do produto: str
-    # preco do produto: float
-    # cód. barra: str (tamanho 8)
+    def __init__(self, nome: str, preco: float, codbar: str):
+        self.__nome = nome
+        self.__preco = preco
+        self.__codbar = None
+        self.set_codbar(codbar)
+
+    def set_codbar(self, codbar):
+        try:
+            if len(codbar) != 8:
+                raise Exception("Cód. Barra deve ter 8 digitos, apenas.")
+            self.__codbar = codbar
+        except Exception as e:
+            print(f"Erro de Valor: {e}")
+
+    def get_produto(self) -> str:
+        return f"{self.__nome}\t{self.__preco}\t{self.__codbar}"
 
 
 class Estoque:
     def __init__(self):
-        self.produtos = []
+        self.produtos: list[Produto] = []
 
-    def adicionar_produto(self, produto: str):  # produto: Produto
+    def adicionar_produto(self, produto: Produto):
         self.produtos.append(produto)
 
     def remover_produto(self):
@@ -26,38 +38,22 @@ class Estoque:
             if not self.produtos:
                 raise IndexError("O estoque está vazio.")
             else:
-                for produto in self.produtos:
-                    print(produto)
+                for index, produto in enumerate(self.produtos, start=1):
+                    print(index, produto.get_produto())
         except IndexError as e:
             print(f"Erro: {e}")
 
 
 estoque = Estoque()
-estoque.adicionar_produto("Macarrão")
-estoque.adicionar_produto("Feijão")
-estoque.adicionar_produto("Margarina")
-estoque.adicionar_produto("Cachaça")
 
-# 4
-print("\nEstoque Atual:")
-estoque.listar_produtos()
+produto1 = Produto("Macarrão", 5.00, "23444345")
+produto2 = Produto("Feijão", 4.50, "09876543")
+produto3 = Produto("Margarina", 6.00, "12345678")
+produto4 = Produto("Cachaça", 18.00, "22323123")
 
-# 3
-print("\nEstoque Atual:")
-estoque.remover_produto()
-estoque.listar_produtos()
+estoque.adicionar_produto(produto1)
+estoque.adicionar_produto(produto2)
+estoque.adicionar_produto(produto3)
+estoque.adicionar_produto(produto4)
 
-# 2
-print("\nEstoque Atual:")
-estoque.remover_produto()
-estoque.listar_produtos()
-
-# 1
-print("\nEstoque Atual:")
-estoque.remover_produto()
-estoque.listar_produtos()
-
-# 0
-print("\nEstoque Atual:")
-estoque.remover_produto()
 estoque.listar_produtos()
